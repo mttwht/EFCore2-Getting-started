7bf4ed848e312aea9393e78302ec98f202494b06
Matthew White <Matthew.White02@gmail.com>
2018-10-03 23:06:21 +0100

    Add an owned type example

---
+++ SamuraiApp.Domain/PersonName.cs
@@ -0,0 +1,15 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class PersonName
+    {
+        public PersonName(string givenName, string surname)
+        {
+            Surname = surname;
+            GivenName = givenName;
+        }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+        public string FullName => $"{GivenName} {Surname}";
+        public string FullNameReverse => $"{Surname}, {GivenName}";
+    }
+}
--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -13,6 +13,7 @@ namespace SamuraiApp.Domain
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public PersonName BetterName { get; set; }
         public List<Quote> Quotes { get; set; }
         public List<SamuraiBattle> SamuraiBattles { get; set; }
         public SecretIdentity SecretIdentity { get; set; }
