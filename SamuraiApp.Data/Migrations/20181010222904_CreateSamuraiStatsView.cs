77085c63db6b0d03770a4f6aafeb6492c25d98a5
Matthew White <Matthew.White02@gmail.com>
2018-10-11 15:06:45 +0100

    Add SamuraiStats view to database

---
+++ SamuraiApp.Data/Migrations/20181010222904_CreateSamuraiStatsView.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class CreateSamuraiStatsView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE VIEW [Dbo].[SamuraiBattleStats]
+AS
+SELECT dbo.Samurais.Name, dbo.SamuraiBattle.SamuraiId,
+    COUNT(dbo.SamuraiBattle.BattleId) AS NumberOfBattles,
+    dbo.EarliestBattleFoughtBySamurai(MIN(dbo.Samurais.Id)) AS EarliestBattle
+FROM dbo.SamuraiBattle
+INNER JOIN dbo.Samurais ON dbo.SamuraiBattle.SamuraiId = dbo.Samurais.Id
+GROUP BY dbo.Samurais.Name, dbo.SamuraiBattle.SamuraiId
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW [Dbo].[SamuraiBattleStats]");
+        }
+    }
+}
